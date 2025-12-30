d04e8e96bacba8fb271dc477617a7f9d3b18e642
bijoyv9 <bijoyv9@gmail.com>
2025-12-30 19:00:46 +0100

    spartan: Remove machine learning blobs
    * liblearningmodule loading is guarded under a debug prop
      in qti perf HAL and libmeters is a liblearningmodule dependency
    
    Change-Id: I97d26e35285c79ef2e5d1d80362a51bd5e3935c7

--- spartan-vendor.mk
+++ spartan-vendor.mk
@@ -721,7 +721,6 @@ PRODUCT_PACKAGES += \
     libkeymasterprovision \
     libkeymasterutils \
     liblbs_core \
-    liblearningmodule \
     libloc_api_v02 \
     libloc_api_wds \
     libloc_core \
@@ -733,7 +732,6 @@ PRODUCT_PACKAGES += \
     liblowi_wifihal \
     liblqe \
     libmdmimgload \
-    libmeters \
     libmidasserviceintf \
     libmidasserviceintf_aidl \
     libminkdescriptor \
